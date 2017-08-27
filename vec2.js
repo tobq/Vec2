571ff6f4583810cb09e72f4e12f8503901e61047
Tobi <tobi.akiny@gmail.com>
2017-08-27 16:12:04 +0100

    Update vec2.js

--- vec2.js
+++ vec2.js
@@ -1,10 +1,21 @@
+/** @constructor */
 function vec2(x, y) {
-	this.x = x || 0;
-	this.y = y || 0;
+	this.x = x;
+	this.y = y;
+}
+
+vec2.radian = function (rads) {
+	return new vec2(Math.cos(rads), Math.sin(rads));
+	// Unit vector for radian angle
+}
+vec2.degree = function (degrees) {
+	return vec2.radian(degrees / 180 * Math.PI);
+	// Unit vector for degree angle
 }
 
 vec2.prototype.copy = function () {
 	return new vec2(this.x, this.y);
+	// Copy vector, so original vector is not mutated by chained methods
 };
 
 vec2.prototype.add = function (vec) {
@@ -57,36 +68,54 @@ vec2.prototype.sMultiply = function (scalar) {
 
 vec2.prototype.dot = function (vec) {
 	return this.x * vec.x + this.y * vec.y;
+	// Dot product
 };
 
 vec2.prototype.mag = function () {
 	return Math.sqrt(this.dot(this));
+	// Magnitude of vector (Pythagoras)
 };
 
 vec2.prototype.unit = function () {
 	return this.copy().sDivide(this.mag());
+	// Unit vector
 };
 
 vec2.prototype.ceil = function () {
 	this.x = Math.ceil(this.x);
 	this.y = Math.ceil(this.y);
 	return this;
+	// Ceil vector components
 };
 
 vec2.prototype.floor = function () {
 	this.x = Math.floor(this.x);
 	this.y = Math.floor(this.y);
+}
+
+vec2.prototype.perp = function () {
+	a = this.x;
+	this.x = this.y;
+	this.y = -a;
 	return this;
+	// Perpendicular vector to vector
 };
 
 vec2.prototype.random = function () {
 	this.x *= Math.random();
 	this.y *= Math.random();
 	return this;
+	// Randomise vector
 };
 
 vec2.prototype.bound = function (component, min, max) {
 	if (this[component] < min) this[component] = Math.min(max, 2 * min - this[component]);
 	else if (this[component] > max) this[component] = Math.max(min, 2 * max - this[component]);
 	else return true;
+	// Bind component to a bounary, which it is flipped about, if outside.
+};
+
+vec2.prototype.angle = function (vec) {
+	return Math.acos(this.dot(vec) / (this.mag() * vec.mag()));
+	// Angle between another vector (radians)
 };
